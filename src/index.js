a8737808315833e2189bcd1ce500513c8884751f
Zmiter <ozmiter@gmail.com>
2021-03-13 16:05:12 +0300

    feature: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array == ''||array == undefined?0:((array[0] ==undefined)?0:array.sort((a, b) => a-b)[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array == ''||array == undefined?0:((array[0] ==undefined)?0:array.sort((a, b) => a-b).pop());
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array == ''||array == undefined?0:array.reduce((sum, e) => 	{return sum + e;}, 0)/(array.length);
 }
